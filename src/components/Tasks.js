9c1b6ca0d48e5d96b446d02e957228d6825399e2
GNikroo <gina.nikroo@gmail.com>
2022-11-29 11:13:28 +0000

    Update tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,223 +1,194 @@
 import React, { useState } from "react";
  
 const Todo = () => {
- const [showForm, setshowform] = useState(true);
- const [showNew, setshowNew] = useState(true);
- const [showDelete, setshowDelete] = useState(true);
- const [toggleSubmit, settoggleSubmit] = useState(true);
- const [isEditItem, setisEditItem] = useState(null);
- const [showList, setshowList] = useState(true);
-//  const [editMessage, seteditMessage] = useState(false);
- const [deleteMessage, setdeleteMessage] = useState(false);
-//  const [deleteMessagesuccess, setdeleteMessagesuccess] = useState(false);
- const [inputTitle, setinputTitle] = useState("");
- const [inputDesc, setinputDesc] = useState("");
- const [items, setitems] = useState([
-   {
-     id: "001",
-     name: "Default Task",
-     desc: "Default Description",
-     status: false,
-   },
- ]);
+    const [showForm, setShowForm] = useState(false);
+    const [showNew, setShowNew] = useState(true);
+    const [showDelete, setShowDelete] = useState(true);
+    const [toggleSubmit, setToggleSubmit] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
+    const [showList, setShowList] = useState(true);
+    const [deleteMessage, setDeleteMessage] = useState(false);
+    const [inputTitle, setInputTitle] = useState("");
+    const [inputDesc, setInputDesc] = useState("");
+    const [items, setItems] = useState([
+        {
+            id: "001",
+            name: "Write a short title for your task",
+            desc: "Put the details here! Provide a description, url, another list... you name it!",
+            status: false,
+        },
+    ]);
+    
+    const handleInput = (e) => {
+        setInputTitle(e.target.value);
+    };
+    const handleInputdesc = (e) => {
+        setInputDesc(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        setShowList(true);
+        setShowNew(true);
+        e.preventDefault();
+        if (!inputTitle || !inputDesc) {
+            alert("Please complete the form!");
+            showList(false);
+        } else if (inputTitle && !toggleSubmit) {
+            setItems(
+                items.map((elem) => {
+                    if (elem.id === isEditItem) {
+                        return { ...elem, name: inputTitle, desc: inputDesc };
+                    }
+                    return elem;
+                })
+            ); 
+            setInputTitle("");
+            setInputDesc("");
+            setToggleSubmit(true);
+            setShowForm(false);
+            setShowDelete(true);
+        } else {
+            const allInputTitle = {
+                id: new Date().getTime().toString(),
+                name: inputTitle,
+                desc: inputDesc,
+            };
+            setItems([allInputTitle, ...items]);
+            setInputTitle("");
+            setInputDesc("");
+            setShowForm(false);
+        }
+    };    
+
+    const handleDelete = (index) => {
+        console.log(index);
+        const updatedItems = items.filter((elem) => {
+            return index !== elem.id;
+        });
+        setDeleteMessage(true);
  
- //   HANDLING INPUT FIELDS
- const handleInput = (e) => {
-   setinputTitle(e.target.value);
- };
- const handleInputdesc = (e) => {
-   setinputDesc(e.target.value);
- };
- //   HANDLING INPUT FIELDS
+        setTimeout(() => {
+            setItems(updatedItems);
+            setDeleteMessage(false);
+        }, 2000);
+    };
+
+    const handleEdit = (id) => {
+        setShowList(false);
+        setShowDelete(false);
+        setShowNew(false);
+        setShowForm(true); 
+        setToggleSubmit(false);
+        let newEditItem = items.find((elem) => {
+            return elem.id === id;
+        });
+        setInputTitle(newEditItem.name);
+        setInputDesc(newEditItem.desc);
  
- //   SUBMITTING FORM
- const handleSubmit = (e) => {
-   setshowList(true);
-   setshowNew(true);
- 
-   e.preventDefault();
-   if (!inputTitle || !inputDesc) {
-     alert("fill data");
-     showList(false);
-   } else if (inputTitle && !toggleSubmit) {
-     setitems(
-       items.map((elem) => {
-         if (elem.id === isEditItem) {
-           return { ...elem, name: inputTitle, desc: inputDesc };
-         }
-         return elem;
-       })
-     );
- 
-     setinputTitle("");
-     setinputDesc("");
-     settoggleSubmit(true);
-     setshowform(false);
-     setshowDelete(true);
-   } else {
-     const allinputTitle = {
-       id: new Date().getTime().toString(),
-       name: inputTitle,
-       desc: inputDesc,
-     };
-     setitems([allinputTitle, ...items]);
-     setinputTitle("");
-     setinputDesc("");
-     setshowform(false);
-   }
- };
- //   SUBMITTING FORM
- 
- //   DELETE
- const handleDelete = (index) => {
-   console.log(index);
-   const updatedItems = items.filter((elem) => {
-     return index !== elem.id;
-   });
-   setdeleteMessage(true);
- 
-   setTimeout(() => {
-     setitems(updatedItems);
-     setdeleteMessage(false);
-   }, 2000);
-//    setdeleteMessagesuccess(false);
- };
- //   DELETE
- 
- //   EDIT
- const handleEdit = (id) => {
-   setshowList(false);
-   setshowDelete(false);
-   setshowNew(false);
-   setshowform(true);
- 
-   settoggleSubmit(false);
-   let newEditItem = items.find((elem) => {
-     return elem.id === id;
-   });
-   setinputTitle(newEditItem.name);
-   setinputDesc(newEditItem.desc);
-   // setshowDelete(true)
- 
-   setisEditItem(id);
-   console.log(newEditItem);
- };
- //   EDIT
- 
- // ADD NEW TASK
- const handleAdd = () => {
-   //   alert("hello")
-   setshowform(true);
-   setshowList(true);
-   setshowNew(false);
- };
- // ADD NEW TASK
- return (
-   <>
-     {showNew ? (
-       <div className="container">
-         <div className="col-12 text-end">
-           <button className="btn btn-primary " onClick={handleAdd}>
-             New
-           </button>
-         </div>
-       </div>
-     ) : (
-       ""
-     )}
- 
-     {showForm ? (
-       <>
-         <div className="container border rounded d-flex justify-content-center shadow p-3 mb-5 bg-white rounded">
-           <div className="row">
-             <div className="text-center">
-               <h2>{toggleSubmit ? "Add Task" : " Edit Task"}</h2>
-             </div>
-             <form className="col-12 p-2" onSubmit={handleSubmit}>
-               <label htmlFor="title" className="my-2">
-                 Enter Title
-               </label>
-               <input
-                 type="text"
-                 name="title"
-                 id="title"
-                 placeholder="title"
-                 className="w-100 my-1 p-2"
-                 onChange={handleInput}
-                 value={inputTitle}
-               />
-               <label className="my-2" htmlFor="description">
-                 Enter
-               </label>
-               <input
-                 type="text"
-                 name="description"
-                 id="description"
-                 placeholder="Description"
-                 className="w-100 my-1 p-2"
-                 onChange={handleInputdesc}
-                 value={inputDesc}
-               />
-               {/* <div className="text-center"> */}
-               {toggleSubmit ? (
-                 <button className="btn btn-primary my-2">Save</button>
-               ) : (
-                 <button className="btn btn-primary my-2">Update</button>
-               )}
-               {/* </div> */}
-             </form>
-           </div>
-         </div>
-       </>
-     ) : (
-       ""
-     )}
- 
-     {showList ? (
-       <div className="container py-2 ">
-         {deleteMessage ? (
-           <p className="text-center text-danger">Item Deleted Successfully</p>
-         ) : (
-           ""
-         )}
-         {items.map((elem, index) => {
-           return (
-             <div
-               className="row border rounded shadow p-3 mb-3 bg-white rounded  p-2"
-               key={elem.id}
-             >
-               <div className="col-12 d-flex justify-content-between align-items-center">
-                 <div>
-                   <h4>{elem.name}</h4>
-                   <p>{elem.desc}</p>
-                 </div>
-                   <button
-                     className="btn btn-primary mx-2"
-                     onClick={() => handleEdit(elem.id)}
-                   >
-                     Edit
-                   </button>
-                   {showDelete ? (
-                     <button
-                       className="btn btn-danger mx-2"
-                       onClick={() => handleDelete(elem.id)}
-                     >
-                       Delete
-                     </button>
-                   ) : (
-                     ""
-                   )}
-                 </div>
-               </div>
-            
-           );
-         })}
-       </div>
-     ) : (
-       ""
-     )}
-   </>
- );
+        setIsEditItem(id);
+        console.log(newEditItem);
+    };
+
+    const handleAdd = () => {
+        setShowForm(true);
+        setShowList(true);
+        setShowNew(false);
+    };
+
+    return (
+        <div>
+            {showNew ? (
+                <div className="container">
+                    <div className="col-12 text-end pb-2">
+                        <button className="btn btn-sm btn-primary" onClick={handleAdd}>
+                            New
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}        
+            {showForm ? (
+                <div>
+                    <div className="container border rounded d-flex justify-content-center shadow p-3 mb-2 bg-white rounded">
+                        <div className="row">
+                            <div className="text-center">
+                                <h2 className="fs-4">{toggleSubmit ? "Add Task" : " Edit Task"}</h2>
+                            </div>
+                            <form className="col-12 p-2" onSubmit={handleSubmit}>
+                                <label htmlFor="title" className="my-2">
+                                    Title
+                                </label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    placeholder="Title"
+                                    className="w-100 my-1 p-2"
+                                    onChange={handleInput}
+                                    value={inputTitle}
+                                />
+                                <label className="my-2" htmlFor="description">
+                                    Description
+                                </label>
+                                <input
+                                    type="text"
+                                    name="description"
+                                    id="description"
+                                    placeholder="Description"
+                                    className="w-100 my-1 p-2"
+                                    onChange={handleInputdesc}
+                                    value={inputDesc}
+                                />
+                                {toggleSubmit ? (
+                                    <button className="btn btn-sm btn-primary my-2">Save</button>
+                                ) : (
+                                    <button className="btn btn-sm btn-primary my-2">Update</button>
+                                )}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}        
+            {showList ? (
+                <div className="container py-2 ">
+                    {deleteMessage ? (
+                        <p className="text-center text-danger">Task Deleted</p>
+                    ) : (
+                        ""
+                    )}
+                    {items.map((elem, index) => {
+                        return (
+                            <div className="row border rounded shadow p-3 mb-3 bg-white rounded  p-2" key={elem.id}>
+                                <div className="col-12 sm-col-6 d-flex justify-content-between align-items-center">
+                                    <div className="text-start">
+                                        <h4 className="fs-4">{elem.name}</h4>
+                                        <p className="fs-6 fw-light">{elem.desc}</p>
+                                    </div>
+                                    <div>
+                                        <button className="btn btn-sm btn-primary mx-2" onClick={() => handleEdit(elem.id)}>
+                                            Edit
+                                        </button>
+                                        {showDelete ? (
+                                            <button className="btn btn-sm btn-danger mx-2" onClick={() => handleDelete(elem.id)}>
+                                                Delete
+                                            </button>
+                                        ) : (
+                                            ""
+                                        )}
+                                    </div>
+                                </div>
+                            </div>                            
+                        );
+                    })}
+                </div>
+            ) : (
+                ""
+            )}
+        </div>
+    );
 };
  
 export default Todo;
